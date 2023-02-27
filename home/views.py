0aeafb55b0c52ae7b3046a0a292461a3b82edbd0
Alireza Mardalishahi <alimardalishahi8013@gmail.com>
2023-02-27 20:52:35 +0330

    add blue print some first setting

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views import View
+# Create your views here.
+
+class HomeView(View):
+    def get(self,request):
+        return render(request,'home/home.html')
