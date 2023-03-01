7e3cabd4897a35e7de1d729e2cb869986652fd1d
Alireza Mardalishahi <alimardalishahi8013@gmail.com>
2023-03-01 16:28:25 +0330

    create form for our User to show in admin panel for creat and change user

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,33 +1,34 @@
 from django import forms
 from .models import User
-from  django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-class UserCreationForm(forms.ModelForm):
 
-    password1=forms.CharField(label="password",widget=forms.PasswordInput)
-    password2=forms.CharField(label="confirm password",widget=forms.PasswordInput)
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="confirm password", widget=forms.PasswordInput)
 
     class Meta:
-        model=User
-        fields=['email','phone_number','full_name']
+        model = User
+        fields = ['email', 'phone_number', 'full_name']
 
     def clean_password2(self):
-        cd=self.cleaned_data
-        if cd['password1'] and cd['password2'] and cd['password1'] !=cd['password2']:
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
             raise ValidationError("password must be matched")
         return cd['password2']
 
     def save(self, commit=True):
-        user=super().save(commit=False)
+        user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
 
+
 class UserChangeForm(forms.ModelForm):
-    password=ReadOnlyPasswordHashField(help_text="you can change password by <a href=\" ../password/\">this form<a>")
+    password = ReadOnlyPasswordHashField(help_text="you can change password by <a href=\" ../password/\">this form<a>")
 
     class Meta:
-        model=User
-        fields=['email','phone_number','full_name','password','last_login']
+        model = User
+        fields = ['email', 'phone_number', 'full_name', 'password', 'last_login']
