721334a2c8dc8e07f65719cf4b767bce5e0c2408
Alireza Mardalishahi <alimardalishahi8013@gmail.com>
2023-03-01 14:41:47 +0330

    creat user model in accounts.model.py and setup some function

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255)
+    phone_number = models.CharField(unique=True, max_length=11)
+    full_name = models.CharField(max_length=128)
+
+    is_active = models.BooleanField(default=True)
+    is_admin=models.BooleanField(default=False)
+
+    USERNAME_FIELD='phone_number'
+    REQUIRED_FIELDS = ['email','full_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self,perm,obj=None):
+        return True
+
+    def has_module_perms(self,app_label):
+        return True
+
+    def is_staff(self):
+        return self.is_admin
