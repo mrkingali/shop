11f789fb4082e1af7d7f76caab169ec58df08cbf
Alireza Mardalishahi <alimardalishahi8013@gmail.com>
2023-03-07 14:02:31 +0330

    add verification function in accounts view.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 
+
 class UserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label="password", widget=forms.PasswordInput)
     password2 = forms.CharField(label="confirm password", widget=forms.PasswordInput)
--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
 from utils import send_top_code
-from .models import OtpCode
+from .models import OtpCode, User
 from random import randint
 
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, VerifyCodeForm
 
 
 class UserRegisterView(View):
@@ -35,12 +35,30 @@ class UserRegisterView(View):
 
 
 class UserRegisterVerifyCodeView(View):
-
+    class_form = VerifyCodeForm
 
     def get(self, request):
-        pass
-
-
+        form = self.class_form
+        return render(request, 'accounts/verify.html', {'form': form})
 
     def post(self, request):
-        pass
+        user_session = request.session['user_registration_info']
+        code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+
+        form = self.class_form(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['code'] == code_instance.code:
+                User.objects.create(phone_number=user_session['phone_number']
+                                    , email=user_session['email']
+                                    , full_name=user_session['full_name']
+                                    , password=user_session['password'])
+                code_instance.delete()
+                messages.success(request, "you registered successfully", 'success')
+                return redirect("home:home")
+            else:
+                messages.error(request, 'code was incorrect', 'danger')
+                return redirect('accounts:verify_code')
+        else:
+            messages.error(request, 'form got error', 'danger')
+            return redirect("home:home")
