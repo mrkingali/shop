8c129fd93cc92fda1074d5da3ae80549f27ef390
Alireza Mardalishahi <alimardalishahi8013@gmail.com>
2023-03-09 23:48:27 +0330

    clean some code

--- home/models.py
+++ home/models.py
@@ -18,19 +18,32 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=200)
+
     slug = models.CharField(max_length=200, unique=True)
+
     image = models.ImageField(upload_to='products/%Y/%m/%d/')
+
     description = models.TextField()
     price = models.IntegerField()
     available = models.BooleanField(default=True)
+
     created = models.DateTimeField(auto_now_add=True)
+
     updated = models.DateTimeField(auto_now=True)
+
+
+
 
     class Meta:
         ordering = ('name',)
 
+
+
     def __str__(self):
         return f'name : {self.name} , price : {self.price} , available: {self.available}'
+
+
+
 
     def get_absolute_url(self):
         return reverse('home:product_detail', args=[self.slug, ])
